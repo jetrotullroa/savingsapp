6e7944ba58b3223bbfc9820cfcb1d68cfb4eb094
jetrotullroa <jetrotullroa@gmail.com>
2016-01-13 13:57:48 +0800

    setup resource and add seed for the record

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+@r1 = Record.create!(title: 'Record 1', date: Date.today, amount: 500)
+@r2 = Record.create!(title: 'Record 2', date: Date.today, amount: -100)
